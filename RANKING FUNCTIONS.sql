11ea5de184a525b30f83dc08aa20bb652096ca62
Vivek Kumar Maddeshiya <vivekkumarmaddeshiya25@gmail.com>
2025-01-07 19:54:27 +0530

    Update RANKING FUNCTIONS.sql

--- RANKING FUNCTIONS.sql
+++ RANKING FUNCTIONS.sql
@@ -20,4 +20,79 @@ SELECT
 FROM Sales.Orders) t
 WHERE ranks=1;
 
+-- Find the lowest 2 customers based on thier sales.
+
+SELECT * FROM
+( SELECT 
+	CustomerID,
+	SUM(Sales) totalsales,
+	ROW_NUMBER() OVER(ORDER BY SUM(Sales)) salesrank
+FROM Sales.Orders
+GROUP BY CustomerID ) t
+WHERE salesrank<=2;
+
+-- USE CASE -> ASSIGN UNIQUE IDs
+-- Assigning unique identifier for each row to help paginating
+
+--Paginating -> Process of breaking down large data into small and managable chunks
+
+SELECT
+	ROW_NUMBER() OVER(ORDER BY ORDERID, ORDERDATE) UNIQ_ID, *
+FROM Sales.OrdersArchive;
+
+
+-- Identify Duplicate data in Table Archive and return a clean results without any duplicates.
+
+--NON DUPLICATE RECORDS
+
+SELECT * FROM(
+SELECT
+	ROW_NUMBER() OVER(PARTITION BY ORDERID ORDER BY CREATIONTIME DESC) RowNum, *
+FROM Sales.OrdersArchive) t 
+WHERE RowNum = 1;
+
+--DUPLICATE RECORDS
+
+SELECT * FROM(
+SELECT
+	ROW_NUMBER() OVER(PARTITION BY ORDERID ORDER BY CREATIONTIME DESC) RowNum, *
+FROM Sales.OrdersArchive) t 
+WHERE RowNum > 1;
+
+
+----------------------------------------NTILE------------------------------------------------
+
+--Divides the data into Specified Number of Approximately equal group ka BUCKETS.
+-- SQL RULE:-  Larger Group Comes First
+
+SELECT 
+ OrderDate,OrderID,
+ NTILE(2) OVER(ORDER BY Sales DESC) TwoBucket,
+ NTILE(3) OVER(ORDER BY Sales DESC) ThreeBucket,
+ NTILE(4) OVER(ORDER BY Sales DESC) FourBucket
+FROM Sales.Orders;
+
+
+-- NTILE Use Cases:-
+-- Data Analyst --> Data Segmentation : Divide a dataset into distince subset based on criteria.
+-- Data Engineer -> Equalising Load Process
+
+
+--Segment all orders into 3 category, High Medium and Low.
+
+SELECT *,
+CASE
+	WHEN Bucket = 1 THEN 'HIGH'
+	WHEN Bucket = 2 THEN 'MEDIUM'
+	WHEN Bucket = 3 THEN 'LOW'
+END SalesSegmentation
+FROM
+(SELECT
+	OrderID, Sales,
+	NTILE(3) OVER(ORDER BY SALES) Bucket
+From Sales.Orders) t;
+
+
+
+
 
