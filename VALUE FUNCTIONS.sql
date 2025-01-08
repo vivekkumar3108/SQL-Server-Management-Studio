a9a13746460af745ed4203d164fb45539956cec8
Vivek Kumar Maddeshiya <vivekkumarmaddeshiya25@gmail.com>
2025-01-08 14:50:45 +0530

    Update VALUE FUNCTIONS.sql

--- VALUE FUNCTIONS.sql
+++ VALUE FUNCTIONS.sql
@@ -8,3 +8,18 @@ SELECT *, ROUND(CAST(( Curr_Month_Sales-Prev_Month_Sales ) AS FLOAT )/ Prev_Mont
 FROM Sales.Orders
 GROUP BY MONTH(OrderDate)
 )t;
+
+--IN ORDER TO ANALYZE CUSTOMER LOYALITY RANK CUSTOMER BASED ON AVERAGE DAYS BETWEEN THIER ORDERS
+
+SELECT 
+	CustomerID,
+	AVG(DaysUntilNextOrder) AverageOrderDays,
+	RANK() OVER(ORDER BY COALESCE(AVG(DaysUntilNextOrder),99999)) Ranking
+FROM
+	(SELECT
+		CustomerID, OrderDate CurrentOrderDate,
+		LEAD(OrderDate) OVER(PARTITION BY CustomerID ORDER BY OrderDate) NextOrderDate,
+		DATEDIFF(DAY,OrderDate,LEAD(OrderDate) OVER(PARTITION BY CustomerID ORDER BY OrderDate)) DaysUntilNextOrder
+	FROM Sales.Orders
+)t
+GROUP BY CustomerID;
